e9547561a5911702385147bb5c6781557fdf23d3
haizhiship <haizhiship@163.com>
2017-03-25 21:26:37 +0800

    新闻读取
    通过NNTP协议读取新闻，并分类显示

---
+++ ProjectTest/ReadNews.py
@@ -0,0 +1,25 @@
+#!/user/bin/python
+# coding=utf-8
+
+"""
+@author:出差在外
+contact:898536955@qq.com
+@file:ReadNews.py
+@time:2017/3/25 20:32
+"""
+
+from nntplib import *
+
+s = NNTP('web.aioe.org')
+
+(resp, count, first, last, name) = s.group('comp.lang.python')
+(resp, subs) = s.xhdr('subject', (str(first)+'-'+str(last)))
+
+for subject in subs[-10:]:
+    print(subject)
+
+number = raw_input('Which article do you want to read? ')
+(reply, num, id, list) = s.body(str(number))
+
+for line in list:
+    print(line)
