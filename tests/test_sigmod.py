3d9c123c3d08724af3dc805fcc0d9c9e45247ad1
haizhiship <haizhiship@163.com>
2017-03-25 19:43:32 +0800

    修改挪走的test文件
    修改挪走的test文件,并将Lesson文件夹设置为Package，否则无法import

--- tests/test_sigmod.py
+++ tests/test_sigmod.py
@@ -2,22 +2,22 @@
 # coding=utf-8
 
 import unittest
-import demo.sigmod
+import Lesson.Lesson1.demo.sigmod as sg
 
 
 class MyTestCase(unittest.TestCase):
     def test_sigmod(self):
    # x 为正常值
         x = 4
-        y = demo.sigmod.sigmod(x)
+        y = sg.sigmod(x)
         self.assertEqual(y, 0.982,'methond  test failed')
    # x过大
         x = 10
-        y = demo.sigmod.sigmod(x)
+        y = sg.sigmod.sigmod(x)
         self.assertEqual(y, 1, 'test failed')
   # x过小
         x = -10
-        y = demo.sigmod.sigmod(x)
+        y = sg.sigmod.sigmod(x)
         self.assertEqual(y, 0, 'test failed')
 
 
